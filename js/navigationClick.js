dd25403b225611d294f334970d907fe6f88b2909
Lukasz Lyskawa <lyskawalukasz@gmail.com>
2016-01-02 11:36:53 +0100

    init

---
+++ js/navigationClick.js
@@ -0,0 +1,11 @@
+switchClass()
+{
+    $('.navbar-nav li a').click(function (e) {
+        var $this = $(this);
+        if(!$this.hasClass('active'))
+        {
+            $this.addClass('active');
+        }
+        e.preventDefault();
+    });
+}
