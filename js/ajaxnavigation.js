4ddf1c9aaae3a81af453985615196c0936c1fa41
Lukasz Lyskawa <lyskawalukasz@gmail.com>
2016-01-02 15:55:11 +0100

    123

--- js/ajaxnavigation.js
+++ js/ajaxnavigation.js
@@ -1,63 +1,60 @@
 
-var Navigation = {
-
-    init: function()
-    {
-        checkForFragmentId();
-        $("#projects").mouseup(function(){
-            $.ajax({url: "Projekty.html", success: function(result){
+function navigationinit()
+{
+    checkForFragmentId();
+    $("#projects").mouseup(function(){
+        $.ajax({url: "Projekty.html", success: function(result){
+            $("#content").html(result);
+            $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
+        }});
+    });
+    $("#certificates").mouseup(function(){
+        $.ajax({url: "Certyfikaty.html", success: function(result){
+            $("#content").html(result);
+            alert("certyfikaty");
+        }});
+    });
+    $("#contact").mouseup(function(){
+        $.ajax({url: "Kontakt.html", success: function(result){
+            $("#content").html(result);
+            alert("kontakt");
+        }});
+    });
+}
+function checkForFragmentId(){
+    if(window.location.hash) {
+        var hash = window.location.hash.substring(1);
+        
+        if(hash === "certificates")
+        {
+            $.ajax({url: "Certyfikaty.html", success: function(result){
                 $("#content").html(result);
                 $('pre code').each(function(i, block) {
                     hljs.highlightBlock(block);
-                });
+                });  
             }});
-        });
-        $("#certificates").mouseup(function(){
-            $.ajax({url: "Certyfikaty.html", success: function(result){
-                $("#content").html(result);
-                alert("certyfikaty");
-            }});
-        });
-        $("#contact").mouseup(function(){
+            $("#certificates").addClass("active");
+        }
+        else if (hash === "contact")
+        {
             $.ajax({url: "Kontakt.html", success: function(result){
                 $("#content").html(result);
-                alert("kontakt");
             }});
-        });
-    }
-    function checkForFragmentId(){
-        if(window.location.hash) {
-            var hash = window.location.hash.substring(1);
-            
-            if(hash === "certificates")
-            {
-                $.ajax({url: "Certyfikaty.html", success: function(result){
-                    $("#content").html(result);
-                    $('pre code').each(function(i, block) {
-                        hljs.highlightBlock(block);
-                    });  
-                }});
-                $("#certificates").addClass("active");
-            }
-            else if (hash === "contact")
-            {
-                $.ajax({url: "Kontakt.html", success: function(result){
-                    $("#content").html(result);
-                }});
-                $("#contact").addClass("active");
-            }
-            else if(hash === "projects")
-            {
-                $.ajax({url: "Projekty.html", success: function(result){
-                    $("#content").html(result);
-                }});
-                $("#projects").addClass("active");
-            }
-        } else {
+            $("#contact").addClass("active");
+        }
+        else if(hash === "projects")
+        {
             $.ajax({url: "Projekty.html", success: function(result){
-                    $("#content").html(result);
-                }});
+                $("#content").html(result);
+            }});
             $("#projects").addClass("active");
         }
+    } else {
+        $.ajax({url: "Projekty.html", success: function(result){
+                $("#content").html(result);
+            }});
+        $("#projects").addClass("active");
     }
 }
